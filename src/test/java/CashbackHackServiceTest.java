19bd0eec4ec8df0a13e014e0fe2f726d7406972f
Yana <podolskaya_22@mail.ru>
2024-09-12 22:45:03 +0700

    junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,12 +1,14 @@
-import org.testng.annotations.Test;
+
+import org.junit.Test;
 import ru.netology.service.CashbackHackService;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
 @Test
-   void shouldCalculateCashback() {
+   public void shouldCalculateCashback() {
     CashbackHackService service = new CashbackHackService();
     int amount = 900;
 
@@ -15,4 +17,15 @@ public class CashbackHackServiceTest {
 
     assertEquals(actual, expected);
 }
+
+ @Test
+ public void shouldNotCalculateCashback() {
+  CashbackHackService service = new CashbackHackService();
+  int amount = 1000;
+
+  int actual = service.remain(amount);
+  int expected = 0;
+
+  assertEquals(expected, actual);
+ }
 }
