0f3fcec0f2e5bdf62fd08a6f76f2f6c298f8ae2c
Yana <podolskaya_22@mail.ru>
2024-09-17 11:50:49 +0700

    add test testng

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -5,14 +5,25 @@ import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-@Test
-   void shouldCalculateCashback() {
-    CashbackHackService service = new CashbackHackService();
-    int amount = 900;
+    @Test
+    void shouldCalculateCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
 
-    int actual = service.remain(amount);
-    int expected = 100;
+        int actual = service.remain(amount);
+        int expected = 100;
 
-    assertEquals(actual, expected);
-}
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNotCalculateCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
